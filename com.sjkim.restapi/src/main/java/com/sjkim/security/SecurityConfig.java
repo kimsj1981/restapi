aa9f32ae346b2c013f14ec9e670cdc1187c557e4
sj.kim <sj.kim@HCNPHSJ-KIM001.ko.tesco.org>
2018-09-17 09:57:53 +0900

    ERROR LOG 관련 테이블 및 시퀀스 명 변경, H2 Console 접속 허용

--- com.sjkim.restapi/src/main/java/com/sjkim/security/SecurityConfig.java
+++ com.sjkim.restapi/src/main/java/com/sjkim/security/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -29,6 +30,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable().authorizeRequests().antMatchers("/api/**").hasRole(role).and().httpBasic()
 				.realmName(realm).authenticationEntryPoint(getBasicAuthEntryPoint());
+	}
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/h2-console/**");
 	}
 
 	@Bean
