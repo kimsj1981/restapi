d7d09244da5c2ff35e5e57598f0f3b9134f1230e
sj.kim <sj.kim@HCNPHSJ-KIM001.ko.tesco.org>
2018-09-27 14:17:01 +0900

    UserDetailsService DI

--- com.sjkim.restapi/src/main/java/com/sjkim/security/SecurityConfig.java
+++ com.sjkim.restapi/src/main/java/com/sjkim/security/SecurityConfig.java
@@ -10,10 +10,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
-import com.sjkim.service.UserDetailServiceImpl;
 
 @Configuration
 @EnableWebSecurity
@@ -26,7 +25,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	private String realm;
 
 	@Autowired
-	private UserDetailServiceImpl userDetailService;
+	private UserDetailsService userDetailService;
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
