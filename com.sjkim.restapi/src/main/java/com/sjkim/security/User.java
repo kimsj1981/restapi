6b0be604491abc0711881db934acf0375ff49144
sj.kim <sj.kim@HCNPHSJ-KIM001.ko.tesco.org>
2018-10-02 16:29:56 +0900

    인증 권한 체크

--- com.sjkim.restapi/src/main/java/com/sjkim/security/User.java
+++ com.sjkim.restapi/src/main/java/com/sjkim/security/User.java
@@ -1,12 +1,9 @@
 package com.sjkim.security;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 @SuppressWarnings("serial")
@@ -16,14 +13,15 @@ public class User implements UserDetails {
 	
 	private String password;
 	
-	@Value("${auth.role}")
-	private String role;
+	private List<GrantedAuthority> authorities;
 	
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
-		List<GrantedAuthority> authorities = new ArrayList<>();
-		authorities.add(new SimpleGrantedAuthority("ROLE_" + role));
 		return authorities;
+	}
+	
+	public void setAuthorities(List<GrantedAuthority> authorities) {
+		this.authorities = authorities;
 	}
 
 	@Override
--- com.sjkim.restapi/src/main/java/com/sjkim/service/UserDetailServiceImpl.java
+++ com.sjkim.restapi/src/main/java/com/sjkim/service/UserDetailServiceImpl.java
@@ -1,8 +1,13 @@
 package com.sjkim.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -20,16 +25,24 @@ public class UserDetailServiceImpl implements UserDetailsService {
 	private UserDao userDao;
 	
 	@Override
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		User user = userDao.selectUser(username);
+	public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+		User user = userDao.selectUser(userName);
 		if (user == null) {
-			throw new UsernameNotFoundException("User Name Not Found : " + username);
+			throw new UsernameNotFoundException("User Name Not Found : " + userName);
 		}
-		
+
 		if (logger.isDebugEnabled()) {
 			logger.debug("userName : " + user.getUsername());
 			logger.debug("password : " + user.getPassword());
 		}
+		
+		List<GrantedAuthority> authorities = new ArrayList<>();
+		List<String> roles  = userDao.selectAuthority(userName);
+		for (String role : roles) {
+			logger.debug("role : " + role);
+			authorities.add(new SimpleGrantedAuthority("ROLE_" + role));
+		}
+		user.setAuthorities(authorities);
 		return user;
 	}
 }
